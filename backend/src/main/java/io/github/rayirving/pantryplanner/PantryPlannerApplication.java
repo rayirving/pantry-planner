8973be52e7a1669881fbcb546dcab7b55bba6051
Ray Irving <rayirving.dev@gmail.com>
2026-09-23 23:07:43 -0600

    Add Spring Boot backend with PostgreSQL configuration

---
+++ backend/src/main/java/io/github/rayirving/pantryplanner/PantryPlannerApplication.java
@@ -0,0 +1,13 @@
+package io.github.rayirving.pantryplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PantryPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PantryPlannerApplication.class, args);
+	}
+
+}
